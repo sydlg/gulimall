53fd1320649d82f5233c644d9e928b8db423b92e
cq <18571811477@163.com>
2020-08-24 17:25:48 +0800

    添加商品管理

--- gulimall-product/src/main/java/com/sydlg/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/sydlg/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.sydlg.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,11 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+        
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
 
 
